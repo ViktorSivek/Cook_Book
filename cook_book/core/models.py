3c5011a52f2b47bd5d115f0102bb679180ff8f47
Viktor Sívek <sivv01@vse.cz>
2022-11-01 23:27:36 +0100

    posts

--- cook_book/core/models.py
+++ cook_book/core/models.py
@@ -6,6 +6,8 @@ from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth import get_user_model
 from static import images
+import uuid
+from datetime import datetime
 
 User = get_user_model()
 
@@ -19,3 +21,21 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user
+
+class LikePost(models.Model):
+    post_id = models.CharField(max_length=500)
+    username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
