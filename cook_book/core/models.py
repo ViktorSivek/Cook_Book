f630fe172fe4edd5fc34ca24877f7643e4ce1be5
Viktor Sívek <sivv01@vse.cz>
2022-11-13 20:28:08 +0100

    delete shits

--- cook_book/core/models.py
+++ cook_book/core/models.py
@@ -5,7 +5,6 @@ from email.policy import default
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth import get_user_model
-from static import images
 import uuid
 from datetime import datetime
 
